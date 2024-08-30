2d62fa5830865de3613d0018505088ded45bf6e0
LeJo <jorgesebastien@gmail.com>
2024-08-31 01:22:35 +0200

    feat(docker): init mysql database

--- localstack.d/init-aws.sh
+++ localstack.d/init-aws.sh
@@ -2,11 +2,11 @@
 
 ### INIT DDB
 awslocal dynamodb create-table \
-  --table-name prompt_paylaod \
+  --table-name prompt_payload \
   --key-schema AttributeName=uuid,KeyType=HASH \
   --attribute-definitions AttributeName=uuid,AttributeType=S \
   --billing-mode PAY_PER_REQUEST
 
 awslocal dynamodb put-item \
-  --table-name archiveRequest \
+  --table-name prompt_payload \
   --item '{"uuid": {"S": "e63a633a-42f6-4bb7-ae31-3c6716e667b0"},"user_uuid": {"S": "5dec90a7-ae81-44f7-b212-204efff641f9"},"sessions_per_week": {"N": "4"},"place": {"S": "gym"},"equipment": {"L": [{"S": "kettel-bell"},{"S": "weight"}]},"video": {"BOOL": true},"additional information": {"S": "add more information here"}}'
