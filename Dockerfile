976de9d35b5a1275b2f2f5fd7b9bdded45765253
LeJo <jorgesebastien@gmail.com>
2024-08-31 20:37:43 +0200

    feat(db): first sql request in mysql database

--- Dockerfile
+++ Dockerfile
@@ -10,14 +10,11 @@ COPY go.sum .
 RUN go mod download
 
 COPY . .
-
-RUN go build -o /myapp ./cmd/web
- 
+RUN go build -o /myapp ./cmd/main.go
 FROM alpine:latest as run
 
 # Copy the application executable from the build image
 COPY --from=build /myapp /myapp
-
 WORKDIR /app
 EXPOSE 8080
 CMD ["/myapp"]
--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,63 @@
 package main
 
-import "fmt"
+import (
+  "fmt"
+  "database/sql"
+  "time"
+
+  _ "github.com/go-sql-driver/mysql" 
+)
+
+type User struct {
+  Uuid string
+  Firstname string
+  Lastname string
+  Email string
+  password string
+}
 
 func main() {
   fmt.Printf("Hello world")
+  users, err := retrieveUsers()
+  if err != nil {
+    panic(err)
+  }
+  fmt.Printf("%v", users)
+}
+
+func retrieveUsers() ([]User, error) {
+  db, err := sql.Open("mysql", "root:password@tcp(localhost:3306)/sport_track_go")
+  if err != nil {
+	  panic(err)
+  }
+  // See "Important settings" section.
+  fmt.Println(db)
+  db.SetConnMaxLifetime(time.Minute * 3)
+  db.SetMaxOpenConns(10)
+  db.SetMaxIdleConns(10)
+  pingErr := db.Ping()
+    if pingErr != nil {
+        panic(pingErr)
+    }
+  fmt.Println("Connected!")
+  var users []User
+  rows, err := db.Query("SELECT * from Users")
+  if err != nil {
+    panic(err)
+  }
+
+  defer rows.Close()
+
+  for rows.Next() {
+    var user User
+    if err := rows.Scan(&user.Firstname, &user.Lastname, &user.password, &user.Email, &user.Uuid); err != nil {
+      return nil, fmt.Errorf("%v", err)
+    }
+    users = append(users, user)
+  }
+
+  if err := rows.Err(); err != nil {
+    return nil, fmt.Errorf("%v", err)
+  } 
+  return users, nil
 }
